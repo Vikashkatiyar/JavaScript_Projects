90a752e50654d5413ff1a48ca3c27968c2a4826f
Vikash Katiyar <rishabktr018@gmail.com>
2023-12-16 02:15:32 +0530

    Project 1,2

---
+++ Projects/02_BMI_Calculator/script.js
@@ -0,0 +1,25 @@
+const form=document.querySelector('form');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const height=parseInt(document.querySelector('#height').value);
+    const weight=parseInt(document.querySelector('#weight').value);
+    const results=document.querySelector('#results');
+
+    if(height==='' || height<0 || isNaN(height)){
+        results.innerHTML=`Please give a valid height ${height}`;
+    }else if(weight==='' || weight<0 || isNaN(weight)){
+        results.innerHTML=`Please give a valid weight ${weight}`;
+    }else{
+        const bmi=(weight/((height*height)/10000)).toFixed(2);
+        if(bmi<18.6){
+            results.innerHTML=`<span>Under Weight: ${bmi}</span>`;
+        }else if(bmi>18.6 && bmi<24.9){
+            results.innerHTML=`<span> Normal Weight: ${bmi}</span>`;
+        }else{
+            results.innerHTML=`<span> Overweight: ${bmi}</span>`;
+        }
+        
+    }
+});
